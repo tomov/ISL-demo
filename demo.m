7990aad6d58ffb1193087b40953ca2bb3fa92866
Momchil Tomov <tomov90@gmail.com>
2019-11-28 17:37:22 -0500

    plotting

--- demo.m
+++ demo.m
@@ -3,4 +3,25 @@ left_is_corr = [1 1 1 1 1 1 1 0 0 0 0 0 0 0 1 1 1 1 1 1 0 0 0 0 0 0];
 
 [u,x,y] = gen(left_is_corr);
 
+figure;
+
+subplot(2,1,1);
+hold on;
+plot(x);
+plot(y, '*');
+plot(left_is_corr);
+legend({'belief', 'choice', 'truth'});
+
 [Xold,Wb,X,W,Xbar] = smooth(u,y);
+
+marg_smooth = sum(Wb .* Xold); % p(xn|y1:T,u1:T)
+post_filter = sum(W(:,end) .* X); % p(x1:T|y1:T,u1:T)
+
+subplot(2,1,2);
+hold on;
+plot(x);
+plot(marg_smooth);
+plot(post_filter);
+legend({'belief', 'marginal (smoothed)', 'posterior (filtered)'});
+
+
