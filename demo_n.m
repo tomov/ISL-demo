870314119ce8c3de52c3b6210e0a2ab05a857b02
Momchil Tomov <tomov90@gmail.com>
2024-04-10 10:06:05 -0400

    r

--- demo_n.m
+++ demo_n.m
@@ -9,6 +9,9 @@ N = 100;
 z_gens = [];
 z_filters = [];
 z_smooths = [];
+r_gens = [];
+r_filters = [];
+r_smooths = [];
 
 for sim = 1:num_sims
     [u,x,y] = gen(left_is_corr);
@@ -27,6 +30,10 @@ for sim = 1:num_sims
     post_filter = sum(W(:,end) .* X); % p(x1:T|y1:T,u1:T)
 
     
+    r_gen = corr(gen_x', x');
+    r_filter = corr(post_filter', x');
+    r_smooth = corr(marg_smooth', x');
+    
     z_gen = atanh(corr(gen_x', x'));
     z_filter = atanh(corr(post_filter', x'));
     z_smooth = atanh(corr(marg_smooth', x'));
@@ -34,18 +41,25 @@ for sim = 1:num_sims
     if isinf(z_gen) || isinf(z_filter) || isinf(z_smooth)
         continue
     end
+      
+    r_gens = [r_gens r_gen];
+    r_filters = [r_filters r_filter];
+    r_smooths = [r_smooths r_smooth];
     
     z_gens = [z_gens z_gen];
     z_filters = [z_filters z_filter];
     z_smooths = [z_smooths z_smooth];
 end
 
+[h,p,ci,stats] = ttest(z_gens);
+fprintf('gen: r = %.2f +- %.2f, T(%d) = %.2f, p = %e\n', mean(r_gens), std(r_gens) / sqrt(length(r_gens)), stats.df, stats.tstat, p);
+
 
 [h,p,ci,stats] = ttest(z_filters, z_gens);
-fprintf('filter vs. gen: T(%d) = %.2f, p = %e\n', stats.df, stats.tstat, p);
+fprintf('filter vs. gen: r = %.2f +- %.2f, T(%d) = %.2f, p = %e\n', mean(r_filters), std(r_filters) / sqrt(length(r_filters)), stats.df, stats.tstat, p);
 
 [h,p,ci,stats] = ttest(z_smooths, z_gens);
-fprintf('smooth vs. gen: T(%d) = %.2f, p = %e\n', stats.df, stats.tstat, p);
+fprintf('smooth vs. gen: r = %.2f +- %.2f, T(%d) = %.2f, p = %e\n', mean(r_smooths), std(r_smooths) / sqrt(length(r_smooths)), stats.df, stats.tstat, p);
 
 [h,p,ci,stats] = ttest(z_smooths, z_filters);
 fprintf('smooth vs. filter: T(%d) = %.2f, p = %e\n', stats.df, stats.tstat, p);
